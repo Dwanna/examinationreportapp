74c61c9f3655010a05c346be0ccec64681e13465
Dwanna <ojeaburuayodeji@gmail.com>
2021-03-01 03:13:27 +0000

    Initial commit

---
+++ src/test/java/com/examinationreport/examinationreportapp/ExaminationreportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examinationreport.examinationreportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationreportappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
